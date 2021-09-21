dbd0fd115a266c4afdd0b54ac6af9de478ee9310
Xel4ek <l-baal@yandex.ru>
2021-09-21 23:16:38 +0300

    init

--- apps/mobile/src/app/app.component.ts
+++ apps/mobile/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'gomoku-root',
+    selector: 'gomoku-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
