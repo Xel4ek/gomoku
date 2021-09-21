c21b5227ae12cab0b5a8c7b1a8f85ee59163bebe
Xel4ek <l-baal@yandex.ru>
2021-09-22 00:01:56 +0300

    routing

--- apps/mobile/src/app/app-routing.module.ts
+++ apps/mobile/src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './core/components/welcome/welcome.component';
+import { AboutComponent } from './core/components/about/about.component';
+import { SettingsComponent } from './core/components/settings/settings.component';
+import { GameComponent } from './core/components/game/game.component';
 const routes: Routes = [
   {
-    path: '',
+    path: 'welcome',
     component: WelcomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'welcome',
   },
 ];
 @NgModule({
