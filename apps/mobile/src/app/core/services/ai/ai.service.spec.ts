79c477027851707339999e30fa967f1e6c391b54
cyberforge12 <50057691+cyberforge12@users.noreply.github.com>
2021-09-25 14:27:57 +0300

    added AiService

--- apps/mobile/src/app/core/services/ai/ai.service.spec.ts
+++ apps/mobile/src/app/core/services/ai/ai.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AiService } from './ai.service';
+import { Board } from "../board";
 
 describe('AiService', () => {
   let service: AiService;
+  let board: Board;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    board = new Board(19);
+    TestBed.configureTestingModule({
+    });
     service = TestBed.inject(AiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should test', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should minimax', () => {
+    board.generateRandomMatrix();
+    expect(service.minimax(board, 3, true, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY, 3)).toBeDefined();
   });
 });
--- apps/mobile/src/app/core/services/ai/ai.service.ts
+++ apps/mobile/src/app/core/services/ai/ai.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Board } from "../board";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AiService {
+
+  minimax(board: Board, depth: number, maximizing: boolean, alpha: number, beta: number, offset: number): number {
+    if (depth === 0 || board.full || board.win) {
+      return this.positionEvaluation(board);
+    }
+    if (maximizing) {
+      let maxEval = Number.NEGATIVE_INFINITY;
+      for (const action of board.generateActions(offset)) {
+        const board_new = new Board(board.size);
+        Object.assign(board_new, board);
+        board_new.move(maximizing, action.col, action.row);
+        const posEval = this.minimax(board_new, depth - 1, !maximizing, alpha, beta, offset);
+        maxEval = Math.max(maxEval, posEval);
+        alpha = Math.max(alpha, posEval);
+        if (beta <= alpha) break;
+      }
+      return maxEval;
+    } else {
+      let minEval = Number.POSITIVE_INFINITY;
+      for (const action of board.generateActions(offset)) {
+        const board_new = new Board(board.size);
+        Object.assign(board_new, board);
+        board_new.move(maximizing, action.col, action.row);
+        const posEval = this.minimax(board_new, depth - 1, !maximizing, alpha, beta, offset);
+        minEval = Math.min(minEval, posEval);
+        beta = Math.min(beta, posEval);
+        if (alpha <= beta) break;
+      }
+      return minEval;
+    }
+  }
+
+  positionEvaluation(board: Board): number {
+    console.log(board);
+    return Math.random();
+  }
 
 }
