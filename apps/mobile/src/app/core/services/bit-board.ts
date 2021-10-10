96c25569ae6486430bd6a2857e4ab95a75bfb455
cyberforge12 <50057691+cyberforge12@users.noreply.github.com>
2021-10-11 01:23:28 +0300

    added BitBoard.move

--- apps/mobile/src/app/core/services/bit-board.ts
+++ apps/mobile/src/app/core/services/bit-board.ts
@@ -1,5 +1,6 @@
 import { BigInteger } from "@angular/compiler/src/i18n/big_integer";
 import { of } from "rxjs";
+import { InvalidMoveError } from "./board";
 
 export class BitBoard {
   winCount = 5;
@@ -20,7 +21,7 @@ export class BitBoard {
   };
   masks = {
     win: BigInt("0b11111"),
-  }
+  };
   nextWhiteMove: boolean;
   win: boolean;
 
@@ -36,7 +37,7 @@ export class BitBoard {
     const str = board.toString(2).split('').reverse();
     for (let row = 0; row < this.size; row++) {
       matrix.push(str
-        .slice(row * this.size, (row + 1) * this.size).join(''))
+        .slice(row * this.size, (row + 1) * this.size).join(''));
     }
     return matrix.join('\n');
   }
@@ -52,13 +53,43 @@ export class BitBoard {
           || (mask & this.boards.max.rotate45l) === mask
           || (mask & this.boards.max.rotate45r) === mask
           || (mask & this.boards.max.rotate90) === mask) {
-          console.log(`${row} ${col} WIN`)
-          return {row: row, col: col};
+          console.log(`${row} ${col} WIN`);
+          return { row: row, col: col };
         }
         mask <<= BigInt(1);
       }
       mask <<= BigInt(this.winCount - 1);
     }
     return {};
+  }
+
+  move(isMax: boolean, col: number, row: number) {
+    if (col >= this.size || row >= this.size || col < 0 || row < 0) {
+      throw new InvalidMoveError(`Cell out of board`);
+    }
+    if (isMax !== this.nextWhiteMove) {
+      throw new InvalidMoveError(`Wrong player`);
+    }
+    const idx = row * this.size + col;
+    const moveMask = BigInt(1) << BigInt(idx);
+    this.checkMove(moveMask);
+    if (isMax) {
+      this.boards.max.orig |= moveMask;
+    } else {
+      this.boards.min.orig |= moveMask;
+    }
+    this.nextWhiteMove = !this.nextWhiteMove;
+    this.updateScore(row, col);
+  }
+
+  private checkMove(mask: bigint) {
+    if (this.boards.max.orig & mask || this.boards.min.orig & mask) {
+      throw new InvalidMoveError(`Cell occupied`);
+    }
+  }
+
+  //TODO: implement update score
+  private updateScore(row: number, col: number) {
+    throw { name: "NotImplementedError" };
   }
 }
