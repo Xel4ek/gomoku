59dbf1ff0e204d3a071a9d0df436df694534056d
cyberforge12 <50057691+cyberforge12@users.noreply.github.com>
2021-10-16 01:20:50 +0300

    added BotBoard.detectLines()

--- apps/mobile/src/app/core/services/bit-board.spec.ts
+++ apps/mobile/src/app/core/services/bit-board.spec.ts
@@ -1,11 +1,24 @@
 import { BitBoard } from './bit-board';
 import { Board } from "./board";
+import { BigInteger } from "@angular/compiler/src/i18n/big_integer";
 
 describe('BitBoard', () => {
   let board: BitBoard;
 
   beforeEach(() => {
     board = new BitBoard(8);
+    board.boards.max.orig = BigInt(
+      "0b" +
+      "000000000" +
+      "000000100" +
+      "000000100" +
+      "001111100" +
+      "010000100" +
+      "010000100" +
+      "001111100" +
+      "000000000"
+    );
+
   });
 
   it('should print bitboard', () => {
@@ -48,8 +61,35 @@ describe('BitBoard', () => {
   });
 
   it('should checkWin', () => {
-    const win = BigInt("0b1111100011011000")
+    const win = BigInt("0b1111100011011000");
     board.boards.max.orig |= win;
-    expect(board.checkMask(board.masks.win)).toEqual({row: 1, col: 3});
+    expect(board.checkMask(board.masks.five, true)).toEqual({ row: 1, col: 3 });
+    console.log(board.printBitBoard(board.boards.max.orig));
+    console.log(board.printBitBoard(board.boards.max.rotate90));
   });
+
+  it('should rotate90 board', () => {
+    console.log(board.printBitBoard(board.boards.max.orig));
+    board.checkMask(board.masks.five, true);
+    console.log(board.printBitBoard(board.boards.max.rotate90));
+  });
+
+  it('should count bits', () => {
+    let count = 0;
+    const i = BigInt("0b111");
+    for (let j = 0; j < 1000; j++) {
+      // console.log(
+      count += board.countBits(i);
+      // );
+    }
+    expect(board.countBits(i)).toEqual(3);
+  });
+
+  it('should count lines', () => {
+    board.player = "max";
+    board.boards.min.orig = BigInt("0b01110")
+    console.log(board.printBitBoard(board.boards.max.orig));
+    console.log(board.detectLines());
+  });
+
 });
--- apps/mobile/src/app/core/services/bit-board.ts
+++ apps/mobile/src/app/core/services/bit-board.ts
@@ -2,9 +2,18 @@ import { BigInteger } from "@angular/compiler/src/i18n/big_integer";
 import { of } from "rxjs";
 import { InvalidMoveError } from "./board";
 
+interface Direction {
+  [prop: string]: number,
+  hor: number,
+  vert: number,
+  diagSW: number,
+  diagSE: number,
+}
+
 export class BitBoard {
   winCount = 5;
   size: number;
+  shift: Direction;
   boards = {
     max: {
       orig: BigInt(0),
@@ -20,16 +29,60 @@ export class BitBoard {
     },
   };
   masks = {
-    win: BigInt("0b11111"),
+    five: BigInt("0b11111"),
+    four1: BigInt("0b1111"),
+    four2: BigInt("0b11011"),
+    Four3: BigInt("0b11101"),
+    three1: BigInt("0b111"),
+    three2: BigInt("0b1101"),
+    three3: BigInt("0b1011"),
+    three4: BigInt("0b10101"),
+    three5: BigInt("0b11001"),
+    three6: BigInt("0b10011"),
   };
-  nextWhiteMove: boolean;
+  player: "max" | "min";
   win: boolean;
 
-  constructor(size: number, nextWhiteMove = true) {
+  constructor(size: number) {
     this.size = size;
-    this.nextWhiteMove = nextWhiteMove;
     this.win = false;
+    //TODO: remove
+    this.player = "max";
     this.setMasks();
+    this.shift = {
+      hor: 1,
+      vert: -(this.size + 1),
+      diagSW: -(this.size + 1) - 1,
+      diagSE: -(this.size + 1) + 1,
+    }
+  }
+
+  isPowerOfTwo(i: bigint) {
+    return i > BigInt(0) && (i & (i - BigInt(1))) === BigInt(0);
+  }
+
+  countBits(v: bigint) {
+    let c: number;
+    for (c = 0; v; c++) {
+      v &= v - BigInt(1);
+    }
+    return c;
+  }
+
+  detectLines() {
+    const len: Direction = {
+      hor: 0,
+      vert: 0,
+      diagSW: 0,
+      diagSE: 0,
+    };
+    Object.keys(len).forEach(dir => {
+      let bits = this.boards[this.player].orig;
+      while ((bits = bits & (bits >> BigInt(this.shift[dir]))) != BigInt(0)) {
+        len[dir]++;
+      }
+    });
+    return len;
   }
 
   printBitBoard(board: BigInt) {
@@ -46,30 +99,33 @@ export class BitBoard {
     const mask = BigInt("0b" + Array(this.winCount).fill("1").join(''));
   }
 
-  checkMask(mask: bigint) {
+  checkMask(mask: bigint, stop: boolean): { dir: string; row: number; col: number }[] {
+    this.rotateBoard();
+    const position: { dir: string, row: number, col: number }[] = [];
     for (let row = 0; row < this.size; row++) {
       for (let col = 0; col <= this.size - this.winCount; col++) {
-        if ((mask & this.boards.max.orig) === mask
-          || (mask & this.boards.max.rotate45l) === mask
-          || (mask & this.boards.max.rotate45r) === mask
-          || (mask & this.boards.max.rotate90) === mask) {
-          console.log(`${row} ${col} WIN`);
-          return { row: row, col: col };
+        for (let [k, v] of Object.entries(this.boards[this.player])) {
+          if ((mask & v) === mask) {
+            position.push({ dir: k, row: row, col: col });
+          }
+          if (stop) {
+            return position;
+          }
         }
         mask <<= BigInt(1);
       }
       mask <<= BigInt(this.winCount - 1);
     }
-    return {};
+    return position;
   }
 
   move(isMax: boolean, col: number, row: number) {
     if (col >= this.size || row >= this.size || col < 0 || row < 0) {
       throw new InvalidMoveError(`Cell out of board`);
     }
-    if (isMax !== this.nextWhiteMove) {
-      throw new InvalidMoveError(`Wrong player`);
-    }
+    // if (isMax !== this.nextWhiteMove) {
+    //   throw new InvalidMoveError(`Wrong player`);
+    // }
     const idx = row * this.size + col;
     const moveMask = BigInt(1) << BigInt(idx);
     this.checkMove(moveMask);
@@ -78,8 +134,13 @@ export class BitBoard {
     } else {
       this.boards.min.orig |= moveMask;
     }
-    this.nextWhiteMove = !this.nextWhiteMove;
+    // this.nextWhiteMove = !this.nextWhiteMove;
     this.updateScore(row, col);
+  }
+
+  private rotateBoard() {
+    // sq' = (((sq >> 3) | (sq << 3)) & 63) ^ 56;
+    this.boards[this.player].rotate90 = this.boards[this.player].orig ^ BigInt(this.size * this.size - this.size);
   }
 
   private checkMove(mask: bigint) {
@@ -92,4 +153,29 @@ export class BitBoard {
   private updateScore(row: number, col: number) {
     throw { name: "NotImplementedError" };
   }
+
+  // getBoradEval() {
+  //   if f(FiveInrow.size()! = 0) then;
+  //   BoardEval + = 100000;
+  //   if f(openfour.size() == 1) then;
+  //   BoardEval + = 15000;
+  //   if f((openthree.size() ≥
+  //   2;
+  // )||
+  //   (Deadfour.size() == 2) || (Deadfour.size() == 1
+  //   openthree.size() == 1;
+  // ))
+  //   then;
+  //   BoardEval + = 10000;
+  //   if f(openthree.size() + jopenthree.size() == 2) then;
+  //   BoardEval + = 5000;
+  //   if f(closedfour.size()! = 0) then;
+  //   BoardEval + = 1000;
+  //   if f(Jclosedfour.size()! = 0) then;
+  //   BoardEval + = 300;
+  //   if f(Cclosedfour.size()! = 0) then;
+  //   BoardEval + = (Cclosedfour.size() ∗ 50;
+  // )
+  //   throw { name: "NotImplementedError" };
+  // }
 }
