e647e6a4496c499ad85084e645cedc9c35b065aa
cyberforge12 <50057691+cyberforge12@users.noreply.github.com>
2022-06-05 12:24:43 +0300

    removed RotatedPattern

--- apps/mobile/src/app/core/services/board/rotated-pattern.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RotatedPattern } from './rotated-pattern';
-
-describe('RotatedPattern', () => {
-  it('should create an instance', () => {
-    expect(new RotatedPattern()).toBeTruthy();
-  });
-});
