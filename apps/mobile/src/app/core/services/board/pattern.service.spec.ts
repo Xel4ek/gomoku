09222391a38677a32466739334c190f2cc6d81e2
cyberforge12 <50057691+cyberforge12@users.noreply.github.com>
2022-06-05 12:36:39 +0300

    pattern.service.spec.ts: removed wrong tests

--- apps/mobile/src/app/core/services/board/pattern.service.spec.ts
+++ apps/mobile/src/app/core/services/board/pattern.service.spec.ts
@@ -1,24 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PatternService } from './pattern.service';
-import { BoardBits } from "./boardBits";
 
 describe('PatternService', () => {
   let service: PatternService;
-  let board: BoardBits;
 
   beforeEach(() => {
-    board = new BoardBits(19, BigInt("0b011111"), 0n, 1n)
     TestBed.configureTestingModule({
     });
-    service = new PatternService(19);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should find Pattern', () => {
-    expect(service.findPatterns(board, "player").length).toBeGreaterThan(0);
-  });
 });
